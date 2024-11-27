ac55b468fb4c8fcb29afbbe9615948ff7a1f40c6
andres acevedo <acevedoandres12@gmail.com>
2024-11-27 18:43:25 -0500

    Contenedor de dependencias

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using CashFlow_API.DAL;
+using CashFlow_API.Domain.Interfaces;
+using CashFlow_API.Domain.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPersonaService, PersonaService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
